c41073d4a6ee78651e7b55f917bef42947c89415
James T <github.jtara1@gmail.com>
2026-02-16 11:58:36 -0800

    feat: created github action
    * feat: github action that leaves 1 comment
    
    FT-1
    
    * feat: HEAD naturallink.ai; added descriptionˆ
    
    * refactor: included dist/ for workflow; made github-token input requirement precondition sooner; set 20 to default node ver; updated meta in manifest
    
    * test: use jest mock timerˆ
    
    * chore: added build artifactˆ
    
    * refactor: specify node ver in package-lock.jsonˆ
    
    * refactor: rm repo access; disabled format checkˆ

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+/**
+ * The main function for the action.
+ *
+ * @returns Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  try {
+    // const token = core.getInput('github-token', { required: true })
+
+    if (github.context.payload.pull_request) {
+      const headResponse = await fetch('https://naturallink.ai', {
+        method: 'HEAD'
+      })
+      const responseHeaders: Record<string, string> = Object.fromEntries(
+        headResponse.headers.entries()
+      )
+      core.info(
+        `HEAD https://naturallink.ai -> ${headResponse.status} ${headResponse.statusText}`
+      )
+      core.info(`Response headers: ${JSON.stringify(responseHeaders)}`)
+    }
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+    else core.setFailed(String(error))
+  }
+}
